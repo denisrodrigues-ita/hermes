4afb55dcd20f9799f2cc93736fc7bf93ccc50e78
denisrodrigues <denisrodrigues.ita@gmail.com>
2025-07-31 21:27:36 +0000

    feat: added getOrThrow in env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,10 +3,12 @@ import { ConfigService } from '@nestjs/config';
 const configService = new ConfigService();
 
 export const ENV = {
-  KEYCLOAK_PUBLIC_KEY: configService.get<string>('KEYCLOAK_PUBLIC_KEY')?.replace(/\n/g, '\n'),
-  KEYCLOAK_ISSUER: configService.get<string>('KEYCLOAK_ISSUER'),
-  KEYCLOAK_CLIENT_ID: configService.get<string>('KEYCLOAK_CLIENT_ID'),
-  KEYCLOAK_URL: configService.get<string>('KEYCLOAK_URL'),
-  KEYCLOAK_REALM: configService.get<string>('KEYCLOAK_REALM'),
-  PORT: configService.get<string>('PORT'),
+  NODE_ENV: configService.getOrThrow<string>('NODE_ENV'),
+  PORT: configService.getOrThrow<string>('PORT'),
+  KEYCLOAK_PUBLIC_KEY: configService.getOrThrow<string>('KEYCLOAK_PUBLIC_KEY')?.replace(/\n/g, '\n'),
+  KEYCLOAK_ISSUER: configService.getOrThrow<string>('KEYCLOAK_ISSUER'),
+  KEYCLOAK_CLIENT_ID: configService.getOrThrow<string>('KEYCLOAK_CLIENT_ID'),
+  KEYCLOAK_URL: configService.getOrThrow<string>('KEYCLOAK_URL'),
+  KEYCLOAK_REALM: configService.getOrThrow<string>('KEYCLOAK_REALM'),
+  KEYCLOAK_CLIENT_SECRET: configService.getOrThrow<string>('KEYCLOAK_CLIENT_SECRET'),
 };
